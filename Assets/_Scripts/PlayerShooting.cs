38c46916313f063ef516e72234ecfe16cb9fc1a4
Khilan <khilan9696@gmail.com>
2016-03-25 14:46:04 -0400

    Game Setup
    Game Setup

---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	//Public Member Variables
+	public Transform flashPoint;
+	public GameObject muzzleFlash;
+	public GameObject bulletImpact;
+	public GameObject explosion;
+
+	public GameController GameController;
+
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start()
+	{
+		this._transform = gameObject.GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Instantiate(this.muzzleFlash, flashPoint.position, Quaternion.identity);
+
+			RaycastHit hit; // stores information from the Ray;
+
+			if (Physics.Raycast(this._transform.position, this._transform.forward, out hit, 50f))
+			{
+				if (hit.transform.gameObject.CompareTag("Barrel"))
+				{
+					Instantiate(this.explosion, hit.point, Quaternion.identity);
+					Destroy(hit.transform.gameObject);
+					this.GameController.ScoreValue += 100;
+				}
+				else {
+
+					Instantiate(this.bulletImpact, hit.point, Quaternion.identity);
+				}
+
+
+
+			}
+
+		}
+	}
+}
